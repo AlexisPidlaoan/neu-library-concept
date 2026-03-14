dc229fd98a56287d76cdc5b5e6bc2207ff9e63ad
Alexis Pidlaoan <pampa4858@gmail.com>
2026-03-14 04:10:07 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3365312849-b4607",
+  "appId": "1:979983399620:web:1aa0c18bc7f2fe2758a791",
+  "apiKey": "AIzaSyA0sdF1joDYiYMr-u3Ic6XeAxejl3EpC68",
+  "authDomain": "studio-3365312849-b4607.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "979983399620"
+};
