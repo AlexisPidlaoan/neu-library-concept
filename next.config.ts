8cf3cefc4e26dbddbdd56e91207d9ae771b7c6ce
Alexis B. Pidlaoan <pampa4858@gmail.com>
2026-03-18 19:50:15 +0800

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -9,13 +9,6 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  // Fix for cross-origin warning in the cloud environment
-  experimental: {
-    allowedDevOrigins: [
-      "9000-firebase-studio-1773460384801.cluster-edb2jv34dnhjisxuq5m7l37ccy.cloudworkstations.dev",
-      "localhost:9002"
-    ],
-  },
   images: {
     remotePatterns: [
       {
